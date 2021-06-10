5b6fca12d505f0a331c9b031b2bcdc09edaa8797
Matteo Monti <matteo.monti@epfl.ch>
2021-06-10 16:09:10 +0200

    Promote `Bytes` to `struct`

--- src/database/bytes.rs
+++ src/database/bytes.rs
@@ -1,3 +1,15 @@
-use drop::crypto::hash::SIZE;
+use drop::crypto::hash::{Digest, SIZE};
 
-pub(crate) type Bytes = [u8; SIZE];
+pub(crate) struct Bytes(pub [u8; SIZE]);
+
+impl Bytes {
+    pub fn empty() -> Self {
+        Bytes([0; SIZE])
+    }
+}
+
+impl From<Digest> for Bytes {
+    fn from(digest: Digest) -> Bytes {
+        Bytes(*digest.as_bytes())
+    }
+}
