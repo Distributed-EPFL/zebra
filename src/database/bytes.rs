246a19f668c4185aaef71389d750976aa9cab509
Matteo Monti <matteo.monti@epfl.ch>
2021-06-10 16:06:56 +0200

    Add `Bytes` as byte array alias

---
+++ src/database/bytes.rs
@@ -0,0 +1,3 @@
+use drop::crypto::hash::SIZE;
+
+pub(crate) type Bytes = [u8; SIZE];
--- src/database/mod.rs
+++ src/database/mod.rs
@@ -1,3 +1,4 @@
 #![allow(dead_code)] // TODO: Remove this attribute, make sure there is no dead code.
 
+mod bytes;
 mod direction;
