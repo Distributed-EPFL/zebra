488abf139998bfd3fdc4b5dbcd6b1083b582e11a
Matteo Monti <matteo.monti@epfl.ch>
2021-06-10 17:06:51 +0200

    Add tests for `Prefix`

--- src/database/prefix.rs
+++ src/database/prefix.rs
@@ -63,3 +63,99 @@ impl PartialEq for Prefix {
             && Path::deepeq(&self.path, &rho.path, self.depth)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use std::vec::Vec;
+
+    fn path_from_directions(directions: &Vec<Direction>) -> Path {
+        let mut path = Path::empty();
+
+        for index in 0..directions.len() {
+            path.set(index as u8, directions[index]);
+        }
+
+        path
+    }
+
+    fn directions_from_path(path: &Path, until: u8) -> Vec<Direction> {
+        (0..until).map(|index| path[index]).collect()
+    }
+
+    fn prefix_from_directions(directions: &Vec<Direction>) -> Prefix {
+        let mut prefix = Prefix::root();
+
+        for &direction in directions {
+            prefix = if direction == Direction::Left {
+                prefix.left()
+            } else {
+                prefix.right()
+            };
+        }
+
+        prefix
+    }
+
+    fn directions_from_prefix(prefix: &Prefix) -> Vec<Direction> {
+        directions_from_path(&prefix.path, prefix.depth())
+    }
+
+    #[test]
+    fn prefix() {
+        use Direction::{Left as L, Right as R};
+        let reference = vec![L, L, L, R, L, L, R, R, R, R, L, R, L, R, L, L];
+
+        let path = path_from_directions(&reference);
+
+        assert_eq!(
+            directions_from_prefix(&Prefix::new(path, reference.len() as u8)),
+            reference
+        );
+        assert_eq!(directions_from_prefix(&Prefix::root()), vec![]);
+        assert_eq!(
+            directions_from_prefix(&prefix_from_directions(&reference)),
+            reference
+        );
+
+        assert!(Prefix::root().contains(&path_from_directions(&vec![L])));
+        assert!(Prefix::root().contains(&path_from_directions(&vec![R])));
+
+        assert!(Prefix::root().left().contains(&path));
+        assert!(!Prefix::root().right().contains(&path));
+
+        assert!(
+            prefix_from_directions(&vec![L, L, L, R, L, L, R]).contains(&path)
+        );
+        assert!(
+            !prefix_from_directions(&vec![L, L, L, R, L, L, L]).contains(&path)
+        );
+
+        assert!(Prefix::new(path, reference.len() as u8).contains(&path));
+        assert!(Prefix::new(path, reference.len() as u8)
+            .right()
+            .contains(&path));
+        assert!(!Prefix::new(path, reference.len() as u8)
+            .left()
+            .contains(&path));
+
+        assert_eq!(Prefix::root(), Prefix::root());
+        assert_eq!(Prefix::root().left(), Prefix::root().left());
+        assert_ne!(Prefix::root().left(), Prefix::root().right());
+        assert_ne!(Prefix::root(), Prefix::root().left());
+
+        assert_eq!(
+            prefix_from_directions(&vec![L, L, L, R, L, L, L]),
+            prefix_from_directions(&vec![L, L, L, R, L, L, L])
+        );
+        assert_ne!(
+            prefix_from_directions(&vec![L, L, L, R, L, L, L]),
+            prefix_from_directions(&vec![L, L, L, R, L, L, R])
+        );
+        assert_ne!(
+            prefix_from_directions(&vec![L, L, L, R, L, L, L]),
+            prefix_from_directions(&vec![L, L, L, R, L, L])
+        );
+    }
+}
