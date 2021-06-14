5f09a785a6d5ef5feb920312f776cb89cf3b2702
Matteo Monti <matteo.monti@epfl.ch>
2021-06-14 16:24:57 +0200

    Derive `Clone` for `Entry`

--- src/database/entry.rs
+++ src/database/entry.rs
@@ -2,6 +2,7 @@ use serde::Serialize;
 
 use super::node::Node;
 
+#[derive(Clone)]
 pub(crate) struct Entry<Key: Serialize, Value: Serialize> {
     pub node: Node<Key, Value>,
     pub references: usize,
