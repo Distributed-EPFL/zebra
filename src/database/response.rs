5833a05f261d1de1e4880556a3174d1ebbd5f7bf
Matteo Monti <matteo.monti@epfl.ch>
2021-06-29 18:23:03 +0200

    Run `cargo fmt`

--- src/database/response.rs
+++ src/database/response.rs
@@ -1,8 +1,8 @@
+use super::action::Action;
 use super::batch::Batch;
 use super::field::Field;
 use super::query::Query;
 use super::transaction::Tid;
-use super::action::Action;
 
 pub struct Response<Key: Field, Value: Field> {
     tid: Tid,
@@ -19,13 +19,20 @@ where
     }
 
     pub fn get(&self, query: &Query) -> Option<&Value> {
-        assert_eq!(query.tid, self.tid, "called `Response::get` with a foreign `Query`");
+        assert_eq!(
+            query.tid, self.tid,
+            "called `Response::get` with a foreign `Query`"
+        );
 
-        let index = self.batch.operations().binary_search_by_key(&query.path, |operation| operation.path).unwrap();
+        let index = self
+            .batch
+            .operations()
+            .binary_search_by_key(&query.path, |operation| operation.path)
+            .unwrap();
         match &self.batch.operations()[index].action {
             Action::Get(Some(holder)) => Some(holder),
             Action::Get(None) => None,
-            _ => unreachable!()
+            _ => unreachable!(),
         }
     }
 }
