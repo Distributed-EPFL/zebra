827460d22260ff89e9d4f8fe8bda78d1c392417b
Matteo Monti <matteo.monti@epfl.ch>
2021-07-05 11:49:31 +0200

    Test `locate`

--- src/database/sync/locate.rs
+++ src/database/sync/locate.rs
@@ -10,7 +10,7 @@ enum Recursion {
     Stop(Label, Label),
 }
 
-fn recur<Key, Value>(
+fn get_siblings<Key, Value>(
     store: &mut Store<Key, Value>,
     label: Label,
 ) -> (u8, (Label, Label))
@@ -32,14 +32,14 @@ where
 
     match recursion {
         Recursion::Recur(child) => {
-            let (dive, (left, right)) = recur(store, child);
+            let (dive, (left, right)) = get_siblings(store, child);
             (dive + 1, (left, right))
         }
         Recursion::Stop(left, right) => (0, (left, right)),
     }
 }
 
-fn path<Key, Value>(store: &mut Store<Key, Value>, label: Label) -> Path
+fn leaf_path<Key, Value>(store: &mut Store<Key, Value>, label: Label) -> Path
 where
     Key: Field,
     Value: Field,
@@ -61,7 +61,148 @@ where
     Key: Field,
     Value: Field,
 {
-    let (dive, (left, right)) = recur(store, label);
-    let common = Prefix::common(path(store, left), path(store, right));
+    let (dive, (left, right)) = get_siblings(store, label);
+    let common =
+        Prefix::common(leaf_path(store, left), leaf_path(store, right));
     common.ancestor(dive)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use crate::database::{
+        interact::{apply, Batch, Operation},
+        tree::Direction::{self, Left as L, Right as R},
+    };
+
+    fn get(store: &mut Store<u32, u32>, label: Label) -> Node<u32, u32> {
+        match store.entry(label) {
+            Occupied(entry) => entry.get().node.clone(),
+            Vacant(..) => panic!("get: node not found"),
+        }
+    }
+
+    fn get_recursive(
+        store: &mut Store<u32, u32>,
+        prefix: Prefix,
+        label: Label,
+    ) -> Label {
+        let mut next = label;
+        for i in prefix.into_iter() {
+            next = match get(store, next) {
+                Node::Internal(left, right) => {
+                    if i == L {
+                        left
+                    } else {
+                        right
+                    }
+                }
+                _ => unreachable!(),
+            };
+        }
+
+        next
+    }
+
+    fn op_set(key: u32, value: u32) -> Operation<u32, u32> {
+        Operation::set(key, value).unwrap()
+    }
+
+    fn check_recursion(
+        store: &mut Store<u32, u32>,
+        prefix: Prefix,
+        label: Label,
+    ) {
+        if !label.is_empty() {
+            match get(store, label) {
+                Node::Internal(left, right) => {
+                    assert_eq!(locate(store, label), prefix);
+                    check_recursion(store, prefix.left(), left);
+                    check_recursion(store, prefix.right(), right);
+                }
+                _ => {}
+            }
+        }
+    }
+
+    fn prefix_from_directions(directions: &Vec<Direction>) -> Prefix {
+        let mut prefix = Prefix::root();
+
+        for &direction in directions {
+            prefix = if direction == Direction::Left {
+                prefix.left()
+            } else {
+                prefix.right()
+            };
+        }
+
+        prefix
+    }
+
+    #[tokio::test]
+    async fn tree() {
+        let store = Store::<u32, u32>::new();
+
+        let batch = Batch::new((0..128).map(|i| op_set(i, i)).collect());
+        let (mut store, root, _) =
+            apply::apply(store, Label::Empty, batch).await;
+
+        let l =
+            get_recursive(&mut store, prefix_from_directions(&vec![L]), root);
+        assert_eq!(locate(&mut store, l), prefix_from_directions(&vec![L]));
+
+        let r =
+            get_recursive(&mut store, prefix_from_directions(&vec![R]), root);
+        assert_eq!(locate(&mut store, r), prefix_from_directions(&vec![R]));
+
+        let ll = get_recursive(
+            &mut store,
+            prefix_from_directions(&vec![L, L]),
+            root,
+        );
+        assert_eq!(locate(&mut store, ll), prefix_from_directions(&vec![L, L]));
+
+        let lr = get_recursive(
+            &mut store,
+            prefix_from_directions(&vec![L, R]),
+            root,
+        );
+        assert_eq!(locate(&mut store, lr), prefix_from_directions(&vec![L, R]));
+
+        let rl = get_recursive(
+            &mut store,
+            prefix_from_directions(&vec![R, L]),
+            root,
+        );
+        assert_eq!(locate(&mut store, rl), prefix_from_directions(&vec![R, L]));
+
+        let rr = get_recursive(
+            &mut store,
+            prefix_from_directions(&vec![R, R]),
+            root,
+        );
+        assert_eq!(locate(&mut store, rr), prefix_from_directions(&vec![R, R]));
+
+        let lll = get_recursive(
+            &mut store,
+            prefix_from_directions(&vec![L, L, R]),
+            root,
+        );
+        assert_eq!(
+            locate(&mut store, lll),
+            prefix_from_directions(&vec![L, L, R])
+        );
+    }
+
+    #[tokio::test]
+    async fn full() {
+        let store = Store::<u32, u32>::new();
+
+        let batch = Batch::new((0..128).map(|i| op_set(i, i)).collect());
+        let (mut store, root, _) =
+            apply::apply(store, Label::Empty, batch).await;
+
+        check_recursion(&mut store, Prefix::root(), root);
+    }
 }
