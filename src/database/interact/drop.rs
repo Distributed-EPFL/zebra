e2970c0eb402d294be8c64f638e7ad9c9460b62f
Matteo Monti <matteo.monti@epfl.ch>
2021-07-08 14:57:24 +0200

    Rationalize `drop` tests

--- src/database/interact/drop.rs
+++ src/database/interact/drop.rs
@@ -18,120 +18,85 @@ where
 mod tests {
     use super::*;
 
-    use crate::database::interact::{apply, Batch, Operation};
+    use crate::database::interact::{apply, Batch};
 
     use rand::seq::IteratorRandom;
     use rand::Rng;
-
-    use std::collections::HashSet;
-
-    fn op_set(key: u32, value: u32) -> Operation<u32, u32> {
-        Operation::set(key, value).unwrap()
-    }
-
-    fn read_labels(
-        store: &mut Store<u32, u32>,
-        label: Label,
-        collector: &mut HashSet<Label>,
-    ) {
-        if !label.is_empty() {
-            collector.insert(label);
-        }
-
-        match label {
-            Label::Internal(..) => {
-                let (left, right) = store.fetch_internal(label);
-                read_labels(store, left, collector);
-                read_labels(store, right, collector);
-            }
-            _ => {}
-        }
-    }
-
-    fn check_size(store: &mut Store<u32, u32>, roots: Vec<Label>) {
-        let mut labels = HashSet::new();
-
-        for root in roots {
-            read_labels(store, root, &mut labels);
-        }
-
-        assert_eq!(store.size(), labels.len());
-    }
 
     #[tokio::test]
     async fn single() {
         let store = Store::<u32, u32>::new();
 
-        let batch = Batch::new((0..128).map(|i| op_set(i, i)).collect());
+        let batch = Batch::new((0..128).map(|i| set!(i, i)).collect());
         let (mut store, root, _) =
             apply::apply(store, Label::Empty, batch).await;
-        check_size(&mut store, vec![root]);
+        store.check_leaks([root]);
 
         drop(&mut store, root);
-        check_size(&mut store, vec![]);
+        store.check_leaks([]);
     }
 
     #[tokio::test]
     async fn double_independent() {
         let store = Store::<u32, u32>::new();
 
-        let batch = Batch::new((0..128).map(|i| op_set(i, i)).collect());
+        let batch = Batch::new((0..128).map(|i| set!(i, i)).collect());
         let (mut store, first_root, _) =
             apply::apply(store, Label::Empty, batch).await;
-        check_size(&mut store, vec![first_root]);
+        store.check_leaks([first_root]);
 
-        let batch = Batch::new((128..256).map(|i| op_set(i, i)).collect());
+        let batch = Batch::new((128..256).map(|i| set!(i, i)).collect());
         let (mut store, second_root, _) =
             apply::apply(store, Label::Empty, batch).await;
-        check_size(&mut store, vec![first_root, second_root]);
+        store.check_leaks([first_root, second_root]);
 
         drop(&mut store, first_root);
-        check_size(&mut store, vec![second_root]);
+        store.check_leaks([second_root]);
 
         drop(&mut store, second_root);
-        check_size(&mut store, vec![]);
+        store.check_leaks([]);
     }
 
     #[tokio::test]
     async fn double_same() {
         let store = Store::<u32, u32>::new();
 
-        let batch = Batch::new((0..128).map(|i| op_set(i, i)).collect());
+        let batch = Batch::new((0..128).map(|i| set!(i, i)).collect());
         let (mut store, first_root, _) =
             apply::apply(store, Label::Empty, batch).await;
-        check_size(&mut store, vec![first_root]);
+        store.check_leaks([first_root]);
 
-        let batch = Batch::new((0..128).map(|i| op_set(i, i)).collect());
+        let batch = Batch::new((0..128).map(|i| set!(i, i)).collect());
         let (mut store, second_root, _) =
             apply::apply(store, Label::Empty, batch).await;
-        check_size(&mut store, vec![first_root, second_root]);
+        store.check_leaks([first_root, second_root]);
 
         drop(&mut store, first_root);
-        check_size(&mut store, vec![second_root]);
+        store.check_leaks([second_root]);
 
         drop(&mut store, second_root);
-        check_size(&mut store, vec![]);
+        store.check_leaks([]);
     }
 
     #[tokio::test]
     async fn double_overlap() {
         let store = Store::<u32, u32>::new();
 
-        let batch = Batch::new((0..128).map(|i| op_set(i, i)).collect());
+        let batch = Batch::new((0..128).map(|i| set!(i, i)).collect());
         let (mut store, first_root, _) =
             apply::apply(store, Label::Empty, batch).await;
-        check_size(&mut store, vec![first_root]);
+        store.check_leaks([first_root]);
 
-        let batch = Batch::new((64..192).map(|i| op_set(i, i)).collect());
+        let batch = Batch::new((64..192).map(|i| set!(i, i)).collect());
         let (mut store, second_root, _) =
             apply::apply(store, Label::Empty, batch).await;
-        check_size(&mut store, vec![first_root, second_root]);
+        store.check_leaks([first_root, second_root]);
 
         drop(&mut store, first_root);
-        check_size(&mut store, vec![second_root]);
+        store.check_leaks([second_root]);
 
         drop(&mut store, second_root);
-        check_size(&mut store, vec![]);
+        store.check_leaks([]);
     }
 
     #[tokio::test]
@@ -145,7 +110,7 @@ mod tests {
             if rng.gen::<bool>() {
                 let keys = (0..1024).choose_multiple(&mut rng, 128);
                 let batch =
-                    Batch::new(keys.iter().map(|&i| op_set(i, i)).collect());
+                    Batch::new(keys.iter().map(|&i| set!(i, i)).collect());
 
                 let result = apply::apply(store, Label::Empty, batch).await;
                 store = result.0;
@@ -157,7 +122,7 @@ mod tests {
                 }
             }
 
-            check_size(&mut store, roots.clone());
+            store.check_leaks(roots.clone());
         }
     }
 }
