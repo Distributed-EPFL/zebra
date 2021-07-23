e6e72fd6434b3398fa8e64d62e6a510b0bd95dd7
Matteo Monti <matteo.monti@epfl.ch>
2021-07-23 18:08:52 +0200

    Remove holder from `Action::Get`

--- src/map/interact/action.rs
+++ src/map/interact/action.rs
@@ -1,10 +1,8 @@
 use crate::{common::store::Field, map::store::Wrap};
 
-use std::rc::Rc;
-
 #[derive(Debug)]
 pub(crate) enum Action<Key: Field, Value: Field> {
-    Get(Option<Rc<Value>>),
+    Get,
     Set(Wrap<Key>, Wrap<Value>),
     Remove,
 }
