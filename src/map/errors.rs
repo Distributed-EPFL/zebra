f6573ce16246641c425d70d4bdb2b5c1f33066d1
Manuel <mvidigueira@gmail.com>
2021-08-24 10:58:20 +0200

    Write level `map` function `get`
    Co-authored-by: Matteo Monti <matteomonti@users.noreply.github.com>

--- src/map/errors.rs
+++ src/map/errors.rs
@@ -1,8 +1,12 @@
+use drop::crypto::hash::HashError as DropHashError;
+
 use snafu::Snafu;
 
 #[derive(Debug, Snafu)]
 #[snafu(visibility(pub(crate)))]
 pub enum MapError {
+    #[snafu(display("failed to hash field: {}", source))]
+    HashError { source: DropHashError },
     #[snafu(display("attempt to operate on an unknown branch"))]
     BranchUnknown,
 }
