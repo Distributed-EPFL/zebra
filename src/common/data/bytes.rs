0020b477ada724a5710eb1798d7d36bcd4d59ad7
Manuel <mvidigueira@gmail.com>
2021-08-24 14:43:05 +0200

    Remove `Bytes::EMPTY`
    Co-authored-by: Matteo Monti <matteomonti@users.noreply.github.com>

--- src/common/data/bytes.rs
+++ src/common/data/bytes.rs
@@ -7,8 +7,6 @@ use std::fmt::{Debug, Error, Formatter, LowerHex};
 #[derive(Clone, Copy, Hash, PartialEq, Eq, PartialOrd, Ord, Serialize)]
 pub(crate) struct Bytes(pub [u8; SIZE]);
 
-pub(crate) const EMPTY: Bytes = Bytes([0; SIZE]);
-
 impl From<Digest> for Bytes {
     fn from(digest: Digest) -> Bytes {
         Bytes(*digest.as_bytes())
